d0bdb8525ed2c1d40543411aa6dc25f6f7e3a90f
khushalk108 <167238314+khushalk108@users.noreply.github.com>
2024-04-17 09:22:57 +0000

    intro to js variables

---
+++ 1_basics/variables.js
@@ -0,0 +1,8 @@
+const accountId = 94849
+let accountEmail = "khushal.kedawat@gmail.com"
+var accountPassword = "849503" 
+ let accountState;
+console.log([accountId,accountEmail,accountPassword]);
+console.table([accountId,accountEmail,accountPassword,accountState]);
+// in var there is problem of scope
+// only use let 
