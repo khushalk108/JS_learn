96020ae2d462a0fe66e189f19cfcd793c680030c
khushalk108 <167238314+khushalk108@users.noreply.github.com>
2024-04-17 09:59:34 +0000

    datatypes

---
+++ 1_basics/datatypes.js
@@ -0,0 +1,10 @@
+"use strict"; // treat all js code as newer version 
+
+let name ="khushal"
+let age = 20
+let isloggedIn = false
+
+// null => standalone value     khaali
+// undefined =>
+// symbol => unique
+// null is an object 
